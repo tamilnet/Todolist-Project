ffaa4ab8c4afedb2dda6c46e7631a5ff39c89c96
Tamil Prabha <prabha.58.ece@gmail.com>
2022-06-15 05:03:08 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,80 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+  useNewUrlParser: true
+});
+const itemsSchema = new mongoose.Schema({
+  name: String
+});
+const Item = mongoose.model("Item", itemsSchema);
+const item1 = new Item({
+  name: "Read"
+});
+const item2 = new Item({
+  name: "Cook"
+});
+const item3 = new Item({
+  name: "Yoga"
+});
+const defaultItems = [item1, item2, item3];
+
+app.get("/", function (req, res) {
+
+  Item.find({}, function (err, foundItems) {
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Successfully inserted default items.");
+        }
+      });
+      res.redirect("/");
+    } else {
+      res.render("list", {
+        listTitle: "Today",
+        newListItems: foundItems
+      });
+  
+    }
+
+  });
+
+
+
+});
+
+app.post("/", function (req, res) {
+
+  const item = req.body.newItem;
+
+  
+});
+
+app.get("/work", function (req, res) {
+  res.render("list", {
+    listTitle: "Work List",
+    newListItems: workItems
+  });
+});
+
+app.get("/about", function (req, res) {
+  res.render("about");
+});
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
